07f2c811913422682399324b76135b29609466ac
Jason Bunnell <jason.bunnell@gmail.com>
2023-11-05 16:51:59 -0500

    Finalized attraction controllers to include Update and Delete methods.

--- controllers/attractions.js
+++ controllers/attractions.js
@@ -4,14 +4,34 @@ const Attraction = require('../models/Attraction');
 // @route   GET /api/v1/attractions
 // @access  Public
 exports.getAttractions = async (req, res, next) => {
-    res.status(200).json({ success: true, msg: 'Show all attractions'});
+    try {
+        const attractions = await Attraction.find();
+        res.status(200).json({ 
+            success: true, 
+            count: attractions.length, 
+            data: attractions 
+        });
+    } catch (err) {
+        res.status(400).json({ success: false });
+    }
 }
 
 // @desc    Get single attraction
 // @route   GET /api/v1/attractions:id
 // @access  Public
 exports.getAttraction = async (req, res, next) => {
-    res.status(200).json({ success: true, msg: `Show attraction ${req.params.id}`});
+    try {
+        const attraction = await Attraction.findById(req.params.id);
+        if(!attraction) {
+            return res.status(400).json({ success: false });
+        }
+        res.status(200).json({ 
+            success: true,
+            data: attraction 
+        });
+    } catch (err) {
+        res.status(400).json({ success: false });
+    }
 }
 
 // @desc    Create new attraction
@@ -33,12 +53,33 @@ exports.createAttraction = async (req, res, next) => {
 // @route   PUT /api/v1/attractions/:id
 // @access  Private
 exports.updateAttraction = async (req, res, next) => {
-    res.status(200).json({ success: true, msg: `Update attraction ${req.params.id}`});
-}
+    try {
+        const attraction = await Attraction.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true
+        });
+    
+        if(!attraction) {
+            return res.status(400).json({ success: false });
+        }
+    
+        res.status(200).json({ success: true, data: attraction });        
+    } catch (err) {
+        res.status(400).json({ success: false });   
+    }
+};
 
 // @desc    Delete attraction
 // @route   DELETE /api/v1/attractions/:id
 // @access  Private
 exports.deleteAttraction = async (req, res, next) => {
-    res.status(200).json({ success: true, msg: `Delete attraction ${req.params.id}`});
+    try {
+        const attraction = await Attraction.findByIdAndDelete(req.params.id);
+        if(!attraction) {
+            return res.status(400).json({ success: false });
+        }
+        res.status(200).json({ success: true, data: {} });
+    } catch (err) {
+        res.status(400).json({ success: false });
+    }
 }
