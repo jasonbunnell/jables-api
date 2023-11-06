7fe3909e11d42d21c04050250d31b25e92484fee
Jason Bunnell <jason.bunnell@gmail.com>
2023-11-06 12:50:09 -0500

    Added GeoCoder.js utils and NodeGeocoder node module to process address field and creat Location object on Attractions model.

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 Colors - adds color options to console
 Morgan - middleware package used to log API calls
 Mongoose - mongodb object modeling for node.js
+[Node Geocode](https://github.com/nchaulet/node-geocoder) - Node.js geocode library
+
+## Services
+[MapQuest Developer](https://developer.mapquest.com/) - Used to get geocode data
--- models/Attraction.js
+++ models/Attraction.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const geocoder = require('../utils/geocoder');
 
 const AttractionSchema = new mongoose.Schema({
     aName: {
@@ -109,6 +110,26 @@ const AttractionSchema = new mongoose.Schema({
 // Create attraction slug from the name
 AttractionSchema.pre('save', function(next) {
     this.slug = slugify(this.aName, { lower: true });
+    next();
+});
+
+// Geocode & create location field
+AttractionSchema.pre('save', async function(next) {
+    const loc = await geocoder.geocode(this.address);
+    
+    this.location = {
+        type: 'Point',
+        coordinates: [loc[0].longitude, loc[0].latitude],
+        formattedAddress: loc[0].formattedAddress,
+        street: loc[0].streetName,
+        city: loc[0].city,
+        state: loc[0].stateCode,
+        zipcode: loc[0].zipcode,
+        country: loc[0].countryCode
+    }
+    
+    // Do not save address in DB
+    this.address = undefined;
     next();
 });
 
