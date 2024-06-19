5f6c7c7c01dec89fd7c82f6068cca372fc5d616d
jason <jason@localhost.localdomain>
2024-06-18 22:11:19 -0400

    Updated seeder to create and delete events from data file.  Updated Event model.

--- seeder.js
+++ seeder.js
@@ -8,17 +8,20 @@ dotenv.config({ path: './config/config.env' });
 
 // Load models
 const Attraction = require('./models/Attraction');
+const Event = require('./models/Event');
 
 // Connect to database
 mongoose.connect(process.env.MONGO_URI, {});
 
 // Read JSON files
 const attractions = JSON.parse(fs.readFileSync(`${__dirname}/_data/attractions.json`, 'utf-8'));
+const events = JSON.parse(fs.readFileSync(`${__dirname}/_data/events.json`, 'utf-8'));
 
 // Import into database
 const importData = async () => {
     try {
         await Attraction.create(attractions);
+        await Event.create(events);
         console.log('Data Imported...'.green.inverse);
         process.exit();
     } catch (err) {
@@ -30,6 +33,7 @@ const importData = async () => {
 const deleteData = async () => {
     try {
         await Attraction.deleteMany();
+        await Event.deleteMany();
         console.log('Data Destroyed...'.red.inverse);
         process.exit();
     } catch (err) {
